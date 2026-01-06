cf8b8e77a8d7c4c88971cc88a9b894752645e1c2
Ariful <arifulmit17@gmail.com>
2026-01-06 21:16:56 +0600

    hero text change

--- src/components/Home/AppSection.tsx
+++ src/components/Home/AppSection.tsx
@@ -8,9 +8,9 @@ export default function AppSection() {
   return (
     <div className='h-full my-20  ' >
         <div className='h-100 rounded-t-3xl flex flex-col p-20 text-white text-center items-center justify-center gap-5 'style={{ backgroundImage: `url(${appstoreimg})` }}>
-           <h1 className='text-3xl'>Access Telikash <br />
+           <h1 className='text-3xl'>Use Payfast <br />
 Anytime, Anywhere!</h1>
-           <p>All the power of niche in your pocket. Schedule, publish and monitir your accounts with ease.</p>
+           <p>All the power of niche in your pocket. Schedule, publish and monitor your accounts with ease.</p>
            <div className="flex items-center justify-center h-2/3 mt-10 gap-5">
             <a href='https://play.google.com/store/apps/details?id=com.telikashuser.alivestation&pcampaignid=web_share' className="w-1/3 h-full">
                  <img  src={googleplay} alt="" />
